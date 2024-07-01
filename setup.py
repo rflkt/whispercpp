4571d48ff1cb7abb8ebfbbd2669cbbec0f0c8dd3
Artem Ivanov <artem@rflkt.io>
2024-07-01 11:21:48 +0200

    build system

--- setup.py
+++ setup.py
@@ -1,63 +1,77 @@
 import os
-from os import path
-from pathlib import Path
-from subprocess import check_output, CalledProcessError
+import pathlib
 
-from setuptools import setup, Command, Extension
-from setuptools.dist import Distribution
-from setuptools.errors import CompileError
-
-WITH_CUDA = os.getenv("WHISPER_CUDA", "0") in ("1", "true", "True", "yes")
+from setuptools import setup, Extension
+from setuptools.command.build_ext import build_ext as build_ext_orig
 
 
-def update_submodules(directory: str):
-    check_output(["git", "init"])
-    check_output(["git", "submodule", "sync", "--recursive"], cwd=directory)
-    check_output(["git", "submodule", "update", "--init", "--recursive"], cwd=directory)
+class CMakeExtension(Extension):
+    def __init__(self, name):
+        # don't invoke the original build_ext for this special extension
+        super().__init__(name, sources=[])
 
 
-class BuildExtension(Command):
+class build_ext(build_ext_orig):
     def run(self):
-        wd = path.dirname(path.abspath(__file__))
-        if not path.exists(path.join(wd, "src", "whispercpp", "api_cpp2py_export.so")):
-            update_submodules(wd)
-            print("Building pybind11 extension...")
-            bazel_script = Path(wd) / "tools" / "bazel"
-            bazel_command = [bazel_script.__fspath__(), "run", "//:extensions"]
-            if self.with_cuda:
-                bazel_command.append("--define=WHISPER_CUDA=1")
-            try:
-                check_output(bazel_command, cwd=wd)
-            except CalledProcessError as e:
-                print(e.output)
-                raise CompileError from e
+        for ext in self.extensions:
+            self.build_cmake(ext)
+        super().run()
 
-    def initialize_options(self):
-        self.with_cuda = None
+    def build_cmake(self, ext):
+        cwd = pathlib.Path().absolute()
 
-    def finalize_options(self):
-        if self.with_cuda is None:
-            self.with_cuda = WITH_CUDA
-        self.with_cuda = self.with_cuda in ("1", "true", "True", "yes", True)
+        # these dirs will be created in build_py, so if you don't have
+        # any python sources to bundle, the dirs will be missing
+        build_temp = pathlib.Path(self.build_temp)
+        build_temp.mkdir(parents=True, exist_ok=True)
+        extdir = pathlib.Path(self.get_ext_fullpath(ext.name))
+        extdir.mkdir(parents=True, exist_ok=True)
 
+        # example of cmake args
+        config = "Debug" if self.debug else "Release"
+        cmake_args = [
+            "-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=" + str(extdir.parent.absolute()),
+            "-DCMAKE_BUILD_TYPE=" + config,
+        ]
 
-class WhisperDistribution(Distribution):
-    def has_ext_modules(self):
-        return True
+        # example of build args
+        build_args = ["--config", config, "--", "-j4"]
+
+        os.chdir(str(build_temp))
+        self.spawn(["cmake", str(cwd)] + cmake_args)
+        if not self.dry_run:
+            self.spawn(["cmake", "--build", "."] + build_args)
+        # Troubleshooting: if fail on line above then delete all possible
+        # temporary CMake files including "CMakeCache.txt" in top level dir.
+        os.chdir(str(cwd))
 
 
 setup(
-    distclass=WhisperDistribution,
+    name="whispercpp",
+    version="0.1.0",
+    # author="Aaron Pham",
+    # author_email="aarnphm@bentoml.com",
+    description="WhisperCPP Python bindings",
+    url="https://github.com/rflkt/whispercpp",
+    license="Apache-2.0",
+    packages=["src.whispercpp"],
     ext_modules=[
-        Extension(
-            "whispercpp",
-            sources=["src/whispercpp/api_cpp2py_export.so"],
-            include_dirs=[
-                "./extern/whispercpp",
-                "./extern/pybind11/include",
-                "./src/whispercpp/",
-            ],
-        )
+        CMakeExtension("cmake-build"),
     ],
-    cmdclass={"build_ext": BuildExtension},
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: POSIX :: Linux",
+        "Topic :: Scientific/Engineering",
+        "Topic :: Software Development",
+        "Programming Language :: Python",
+        "Programming Language :: C++",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+    ],
 )
